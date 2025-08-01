70cf820ed66b35d4fc863eaad6a50f3bbb90645e
ABUBAKARKHAN5376 <abubakarkhan5376@gmail.com>
2025-08-01 06:10:53 -0700

    final

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -58,7 +58,7 @@ export default function Navbar() {
                         item.id ? (
                             <li key={item.id} className="relative cursor-pointer">
                                 <button
-                                    onClick={() => toggleDropdown(item.id)}
+                                    onMouseOver={() => toggleDropdown(item.id)}
                                     data-dropdown-button
                                     className="flex items-center gap-1 cursor-pointer"
                                 >
